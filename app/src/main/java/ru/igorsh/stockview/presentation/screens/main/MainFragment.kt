6cb06d424e5d95f8ffc3ac50e908d0e77c653c5d
Igor <igor.sheplyakov@yandex.ru>
2023-03-21 15:40:06 +0300

    добавил верстку главного экрана

--- app/src/main/java/ru/igorsh/stockview/presentation/screens/main/MainFragment.kt
+++ app/src/main/java/ru/igorsh/stockview/presentation/screens/main/MainFragment.kt
@@ -1,7 +1,13 @@
 package ru.igorsh.stockview.presentation.screens.main
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import ru.igorsh.stockview.R
 
 class MainFragment: Fragment(R.layout.fragment_main) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
