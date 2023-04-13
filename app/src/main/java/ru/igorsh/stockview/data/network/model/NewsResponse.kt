578406ac73a17f63025cd7edc32584b59d9a4914
Igor <igor.sheplyakov@yandex.ru>
2023-04-13 15:15:43 +0300

    удалил NewsResponse

--- app/src/main/java/ru/igorsh/stockview/data/network/model/NewsResponse.kt
+++
@@ -1,8 +0,0 @@
-package ru.igorsh.stockview.data.network.model
-
-import com.google.gson.annotations.SerializedName
-
-data class NewsResponse(
-    @SerializedName("news")
-    val news: List<NewsResponseItem>
-)
